64f1f85f816e6e0b8740a3ae53236510d590cd01
Antoni Realp Serrano <antoni.realp@enti.cat>
2018-12-29 21:00:07 +0100

    Server: sesiones implemented

--- AA5/Practica2.2_Server/main.cpp
+++ AA5/Practica2.2_Server/main.cpp
@@ -7,8 +7,10 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <ctime>
 
-enum ID {LOGIN,SIGNUP,MAPS};
+enum ID {LOGIN, SIGNUP, MAPS, DISCONNECT};
+std::vector<sf::TcpSocket*> sockets;
 
 
 void connection(sf::TcpSocket* socket){
@@ -23,6 +25,8 @@ void connection(sf::TcpSocket* socket){
 
     std::string username, password, playerID;
     int id, coins;
+
+    time_t logInSec, logOutSec;
 
     sf::Packet packet;
 
@@ -111,6 +115,7 @@ void connection(sf::TcpSocket* socket){
     socket->send(packet);
 	//Se pregunta al usuario que mapa quiere jugar. Debe escribir el nombre del mapa
     bool mapSelected = false;
+
     do {
         if(socket->receive(packet)!=sf::Socket::Status::Done){
             std::cout<<"el cliente se ha desconectado"<<std::endl;
@@ -122,15 +127,46 @@ void connection(sf::TcpSocket* socket){
 			//Se comprueba que el mapa escrito exista en la BD
             if(mapName==resultSet->getString("Name")){
                 mapSelected = true;
-                std::cout<<mapName<<std::endl;
                 packet.clear();
-                packet<<true;
-                socket->send(packet);
-                //logInSec = time(0);
+                mapSelected=true;
+                logInSec = time(0);
                 break;
             }
         }
+
+        packet<<mapSelected;
+        socket->send(packet);
+
     }while(!mapSelected);
+
+    socket->receive(packet);
+    packet>>id;
+    if(id==DISCONNECT){
+        for(int it = 0;it<sockets.size();it++){
+            if(sockets[it]==socket){
+                sockets.erase(sockets.begin()+it);
+                break;
+            }
+        }
+
+        tm* logInTime, * logOutTime;
+        char logInBuf [22];
+        char logOutBuf [22];
+    //La funcion strftime nos permite formatear la string con la fecha y hora para que se ajuste al formato de datetime de la bd
+        logInTime=localtime(&logInSec);
+        strftime(logInBuf,22,"%F %X\0",logInTime);
+        std::string logInStr(logInBuf);
+
+        time_t logOutSec = time(0);
+        logOutTime=localtime(&logOutSec);
+        strftime(logOutBuf,22,"%F %X\0",logOutTime);
+        std::string logOutStr(logOutBuf);
+
+        stmt->executeUpdate("INSERT INTO Sesion(PlayerID, LogInTime, LogOutTime) VALUES ("+playerID+",'"+logInStr+"','"+logOutStr+"')");
+
+        std::cout<<"El cliente "<<username<<" se ha desconectado"<<std::endl;
+    }
+
 }
 
 int main()
@@ -138,7 +174,7 @@ int main()
     ///Se inicializan los sockets
     sf::TcpListener dispatcher;
     sf::Socket::Status status = dispatcher.listen(50000);
-    std::vector<sf::TcpSocket*> sockets;
+
 
     if(status != sf::Socket::Done)
     {
